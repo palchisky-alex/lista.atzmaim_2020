eedb4264a2cbc58d88b0f356e42c4ce74d37773d
moromoi <alex.palchisky@gmail.com>
2020-05-05 22:23:02 +0300

    add allure rest 4

--- UI_API_Tests/src/test/java/im/atzma/lista2020/Tests/AccountCreation_UI_API_Tests.java
+++ UI_API_Tests/src/test/java/im/atzma/lista2020/Tests/AccountCreation_UI_API_Tests.java
@@ -1,5 +1,6 @@
 package im.atzma.lista2020.Tests;
 
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Description;
 import org.approvaltests.Approvals;
 import org.testng.Assert;
@@ -7,28 +8,32 @@ import org.testng.annotations.Test;
 
 public class AccountCreation_UI_API_Tests extends TestBase {
 
+    @Attachment
     @Test(priority = 1)
     public void createAccount() {
         Assert.assertEquals(app.accountCreation_UI_API_Helper().createAccount(), 201);
     }
 
+    @Attachment
     @Test(priority = 2)
     public void verifyAccountCreation() {
         Assert.assertEquals(app.accountCreation_UI_API_Helper().verifyAccountCreation(), "/en/calendar");
     }
 
-
+    @Attachment
     @Test(priority = 3)
     public void deleteAccount() {
         Assert.assertEquals(app.accountCreation_UI_API_Helper().deleteAccount(), 401);
     }
 
+    @Attachment
     @Test(priority = 4)
     @Description("verifyAccountDeletion")
     public void verifyAccountDeletion() {
         Assert.assertEquals(app.accountCreation_UI_API_Helper().verifyAccountDeletion(), "/he/login");
     }
 
+    @Attachment
     @Test(priority = 5)
     public void getBusinessTypeJSON() {
         Approvals.verify(app.accountCreation_UI_API_Helper().businessTypeResponse());
