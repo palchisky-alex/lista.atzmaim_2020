f1d12b3f22785e957a721822ccfb4b3ce5ace2b1
moromoi <alex.palchisky@gmail.com>
2020-05-08 22:25:38 +0300

    allure fix

--- UI_API_Tests/src/test/java/im/atzma/lista2020/appmanager/AccountCreation_UI_API_Helper.java
+++ UI_API_Tests/src/test/java/im/atzma/lista2020/appmanager/AccountCreation_UI_API_Helper.java
@@ -1,18 +1,15 @@
 package im.atzma.lista2020.appmanager;
 
 import io.qameta.allure.Allure;
-import io.qameta.allure.AllureLifecycle;
 import io.qameta.allure.Attachment;
-import io.qameta.allure.model.Status;
-import io.qameta.allure.model.StepResult;
 import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
+import org.apache.commons.io.output.WriterOutputStream;
+import org.testng.annotations.Listeners;
 
 import java.io.PrintStream;
 import java.io.StringWriter;
@@ -21,10 +18,8 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
-import static io.restassured.config.RedirectConfig.redirectConfig;
-
+@Listeners(LogListener.class)
 public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
     String key = "7b7a53e239400a13bd6be6c91c4f6c4e";
     String value;
@@ -41,7 +36,7 @@ public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
     DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("HH:mm:ss");
     String currentTime = time.format(dtf2);
 
-    @Attachment
+
     public String businessTypeResponse() {
 
         System.out.println("=== GET BUSINESS TYPE JSON ===");
@@ -55,7 +50,7 @@ public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
         return responseString;
     }
 
-    @Attachment
+
     public int createAccount() {
         Random random = new Random();
         int randomInt = random.nextInt();
@@ -70,10 +65,14 @@ public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
 //        final PrintStream responseVar = new PrintStream(new WriterOutputStream(writerResponse), true);
         String requestTemplatePath = "resources/tpl/http-request.ftl";
         String responseTemplatePath = "resources/tpl/http-response.ftl";
-        RequestSpecification requestSpecification = null;
-        requestSpecification.when().log().all();
+
+        StringWriter writerReqes = new StringWriter();
+        final StringWriter writerResponse = new StringWriter();
+        final PrintStream requestVar = new PrintStream(new WriterOutputStream(writerReqes));
+        final PrintStream responseVar = new PrintStream(new WriterOutputStream(writerResponse), true);
 
         post_response = given(). filters(new CustomAllureRestAssured().setRequestTemplate(requestTemplatePath).setResponseTemplate(responseTemplatePath)).
+
                 header("Content-Type", "application/x-www-form-urlencoded").
                 header("user-agent", "alpalch-qpEzhaOvY0Ecb4e0").
                 header("X-Requested-With", "XMLHttpRequest").
@@ -87,7 +86,7 @@ public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
                 formParam("timezone", "Asia/Jerusalem").
                 formParam("country", "IL").
                 formParam("city", "Tesl Aviv").
-                when().log().all().
+                when().
                 post("/signup-new-account").then().
                 extract().response();
 
@@ -104,13 +103,13 @@ public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
         System.out.println("Create account status: " + post_response.getStatusCode());
         return post_response.getStatusCode();
     }
-    @Attachment
+
     public String verifyAccountCreation() {
         System.out.println("ACCOUNT CREATION RESPONSE MUST BE '/en/calendar'");
         System.out.println("Account creation response: " + accountCreationResponse);
         return accountCreationResponse;
     }
-    @Attachment
+
     public int deleteAccount() {
         System.out.println("=== REMOVE ALL ACCOUNTS, STATUS MUST BE 401 ===");
         System.out.println("=== ACCOUNTS DESIGNED FOR REMOVAL - 1 ");
@@ -131,7 +130,7 @@ public class AccountCreation_UI_API_Helper extends RequestSpecBuilder {
         return delete_response.getStatusCode();
     }
 
-    @Attachment
+
     public String verifyAccountDeletion() {
         System.out.println("=== VERIFY ACCOUNTS DELETION ===");
         System.out.println("=== STATUS MUST BE 302 AND 'LOCATION' VALUE - '/he/login' ===");
