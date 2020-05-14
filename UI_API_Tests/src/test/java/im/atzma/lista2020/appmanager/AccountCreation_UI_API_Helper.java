2d2a00b8804d3078a48d4c8f2b46500c27951332
moromoi <alex.palchisky@gmail.com>
2020-05-14 23:27:08 +0300

    add allure rest 11

--- UI_API_Tests/src/test/java/im/atzma/lista2020/appmanager/AccountCreation_UI_API_Helper.java
+++ UI_API_Tests/src/test/java/im/atzma/lista2020/appmanager/AccountCreation_UI_API_Helper.java
@@ -55,8 +55,8 @@ public class AccountCreation_UI_API_Helper {
         System.out.println("=== CREATE RANDOM ACCOUNT, STATUS MUST BE 201 ===");
         accounts.put(random_for_mail + "@gmail.com", "Pa$$w@rd");
         // filters(new CustomAllureRestAssured().setRequestTemplate(requestTemplatePath).setResponseTemplate(responseTemplatePath)).
-        RestAssured.requestSpecification = new RequestSpecBuilder().build().filter(new AllureRestAssured());
-        post_response = given().filters(new CustomAllureRestAssured()).
+        post_response = given().filter(new AllureRestAssured().
+                setRequestTemplate("").setResponseTemplate("")).
                 header("Content-Type", "application/x-www-form-urlencoded").
                 header("user-agent", "alpalch-qpEzhaOvY0Ecb4e0").
                 header("X-Requested-With", "XMLHttpRequest").
