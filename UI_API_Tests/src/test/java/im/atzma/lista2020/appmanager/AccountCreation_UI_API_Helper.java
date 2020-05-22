dfc629e28e8229d33d4b6bfc81020435b11c6393
moromoi <alex.palchisky@gmail.com>
2020-05-22 16:58:07 +0300

    add allure rest 54

--- UI_API_Tests/src/test/java/im/atzma/lista2020/appmanager/AccountCreation_UI_API_Helper.java
+++ UI_API_Tests/src/test/java/im/atzma/lista2020/appmanager/AccountCreation_UI_API_Helper.java
@@ -71,7 +71,7 @@ public class AccountCreation_UI_API_Helper {
 //        config = config().logConfig(new LogConfig().defaultStream(fileOutPutStream));
 //.filters(new CustomAllureRestAssured(), new RequestLoggingFilter())
 
-            post_response = given().log().all().
+            post_response = given().log().all().filter(new RequestLoggingFilter()).
                     header("Content-Type", "application/x-www-form-urlencoded").
                     header("user-agent", "alpalch-qpEzhaOvY0Ecb4e0").
                     header("X-Requested-With", "XMLHttpRequest").
