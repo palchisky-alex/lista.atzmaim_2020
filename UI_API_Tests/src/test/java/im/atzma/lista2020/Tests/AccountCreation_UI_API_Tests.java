2e0a5303b756677149ff1f4b0d73e1def615fb6c
moromoi <alex.palchisky@gmail.com>
2020-05-26 12:54:58 +0300

    add allure rest 60

--- UI_API_Tests/src/test/java/im/atzma/lista2020/Tests/AccountCreation_UI_API_Tests.java
+++ UI_API_Tests/src/test/java/im/atzma/lista2020/Tests/AccountCreation_UI_API_Tests.java
@@ -4,6 +4,7 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import java.io.FileNotFoundException;
@@ -20,26 +21,56 @@ public class AccountCreation_UI_API_Tests extends TestBase_UI_API {
 
     @Test(priority = 1)
     public void createAccount() {
-        app.accountCreation_UI_API_Helper().createAccount().then().assertThat().statusCode(201);
+        String currentDate = LocalDate.now().toString();
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String currentTime = time.format(dtf2);
+        Random random = new Random();
+        int randomInt = random.nextInt();
+        String random_for_mail = "api_test_ui" + randomInt;
+
+        System.out.println("=== CREATE RANDOM ACCOUNT, STATUS MUST BE 201 ===");
+        Response post_response = given().log().all().filter(new AllureRestAssured()).
+                header("Content-Type", "application/x-www-form-urlencoded").
+                header("user-agent", "alpalch-qpEzhaOvY0Ecb4e0").
+                header("X-Requested-With", "XMLHttpRequest").
+                formParam("added", currentDate + " " + currentTime).
+                formParam("email", random_for_mail + "@gmail.com").
+                formParam("pass", "Pa$$w@rd").
+                formParam("phone", "0547613154").
+                formParam("permit_ads", "true").
+                formParam("business_types", "[1]").
+                formParam("lang", "en").
+                formParam("timezone", "Asia/Jerusalem").
+                formParam("country", "IL").
+                formParam("city", "Tesl Aviv").
+                when().
+                post("/signup-new-account").then().extract().response();
+        assertEquals(post_response.statusCode(), 201);
     }
 
-    @Test(priority = 2)
-    public void verifyAccountCreation() {
-        assertEquals(app.accountCreation_UI_API_Helper().verifyAccountCreation(), "/en/calendar");
-    }
-
-
-    @Test(priority = 3)
-    public void deleteAccount() {
-        app.accountCreation_UI_API_Helper().deleteAccount().then().assertThat().statusCode(401);
-    }
-
-
-
-    @Test(priority = 4)
-    public void verifyAccountDeletion() {
-        assertEquals(app.accountCreation_UI_API_Helper().verifyAccountDeletion(), "/he/login");
-    }
+//    @Test(priority = 1)
+//    public void createAccount() {
+//        app.accountCreation_UI_API_Helper().createAccount().then().assertThat().statusCode(201);
+//    }
+//
+//    @Test(priority = 2)
+//    public void verifyAccountCreation() {
+//        assertEquals(app.accountCreation_UI_API_Helper().verifyAccountCreation(), "/en/calendar");
+//    }
+//
+//
+//    @Test(priority = 3)
+//    public void deleteAccount() {
+//        app.accountCreation_UI_API_Helper().deleteAccount().then().assertThat().statusCode(401);
+//    }
+//
+//
+//
+//    @Test(priority = 4)
+//    public void verifyAccountDeletion() {
+//        assertEquals(app.accountCreation_UI_API_Helper().verifyAccountDeletion(), "/he/login");
+//    }
 
 
 }
